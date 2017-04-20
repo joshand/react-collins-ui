be53ceebba2d9f9ed9e463352421c281f61c4136
kecorbin <kecorbin@cisco.com>
2017-04-20 02:45:16 -0500

    add routes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,12 @@
 import type { Dispatch } from './types';
 import { fetchUsersIfNeeded } from './containers/Home/action';
 import { fetchUserIfNeeded } from './containers/UserInfo/action';
+import { fetchJobsIfNeeded } from './containers/JobsHome/action';
+import { fetchJobIfNeeded } from './containers/JobInfo/action';
 import HomePage from './containers/Home';
+import JobsHomePage from './containers/JobsHome';
 import UserInfoPage from './containers/UserInfo';
+import JobInfoPage from './containers/JobInfo';
 import NotFoundPage from './containers/NotFound';
 
 export default [
@@ -22,6 +26,29 @@ export default [
     loadData: (dispatch: Dispatch, params: Object) => Promise.all([
       dispatch(fetchUserIfNeeded(params.id)),
     ]),
+  },
+  {
+    path: '/jobs',
+    // If the route matches the location.pathname exactly or not (used for index route usually)
+    exact: true,
+    // Add your route component here
+    component: JobsHomePage,
+    loadData: (dispatch: Dispatch, params: Object) => Promise.all([
+      dispatch(fetchJobsIfNeeded(params.id)),
+    ]),
+
+  },
+  {
+    // Define your route path
+    path: '/jobs/:id',
+    // If the route matches the location.pathname exactly or not (used for index route usually)
+    exact: true,
+    // Add your route component here
+    component: JobInfoPage,
+    loadData: (dispatch: Dispatch, params: Object) => Promise.all([
+      dispatch(fetchJobIfNeeded(params.id)),
+    ]),
+
   },
   {
     path: '*',
