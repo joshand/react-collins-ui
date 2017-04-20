ac92a13759bce591e639481d49df3b7f9c8b147c
Josh Anderson <joshand@cisco.com>
2017-04-20 18:26:45 -0500

    updated App to pull external navbar, moved below icon/title

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React from 'react';
-import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+// import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import _ from 'lodash';
@@ -11,7 +11,7 @@ import routes from '../../routes';
 // Import your global styles here
 import '../../theme/normalize.css';
 import styles from './styles.scss';
-// import NavBar from '../../components/NavBar';
+import navbarInstance from '../../components/NavBar';
 
 export default () => {
   // Use it when sub routes are added to any route it'll work
@@ -28,46 +28,18 @@ export default () => {
   );
 
   return (
-    <div id="container-fluid">
-      <div id="Navbar">
-        <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="http://foo.com">React-Bootstrap</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="/jobs">Jobs</NavItem>
-              <NavItem eventKey={2} href="/">Users</NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="/link1">Link Right</NavItem>
-              <NavItem eventKey={2} href="/link2">Link Right</NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+    <div className={styles.App}>
+      <Helmet {...config.app} />
+      <div className={styles.header}>
+        <img src={require('./assets/logo.svg')} alt="Logo" role="presentation" />
+        <h1>{config.app.title}</h1>
       </div>
-      <div className={styles.App}>
-        <Helmet {...config.app} />
-        <div className={styles.header}>
-          <img src={require('./assets/logo.svg')} alt="Logo" role="presentation" />
-          <h1>{config.app.title}</h1>
-        </div>
-        <hr />
+      {navbarInstance}
+      <hr />
 
-        <Switch>
-          {routes.map(route => routeWithSubRoutes(route))}
-        </Switch>
-      </div>
+      <Switch>
+        {routes.map(route => routeWithSubRoutes(route))}
+      </Switch>
     </div>
   );
 };
