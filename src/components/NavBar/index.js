fd31d48395744f3a70af5318d212e73748a34cc3
kecorbin <kecorbin@cisco.com>
2017-04-20 17:00:07 -0500

    add basic navbar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 
-const NavBar = (
+const navbarInstance = (
   <Navbar inverse collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
+        <a href="http://foo.com">React-Bootstrap</a>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavItem eventKey={1} href="/jobs">Jobs</NavItem>
+        <NavItem eventKey={2} href="/">Users</NavItem>
         <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
           <MenuItem eventKey={3.1}>Action</MenuItem>
           <MenuItem eventKey={3.2}>Another action</MenuItem>
@@ -23,11 +23,11 @@ const NavBar = (
         </NavDropdown>
       </Nav>
       <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
+        <NavItem eventKey={1} href="/link1">Link Right</NavItem>
+        <NavItem eventKey={2} href="/link2">Link Right</NavItem>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
 
-export default NavBar;
+export default navbarInstance;
