6debd6d9de41987db799a0dd24fdde55ad5d5c3e
kecorbin <kecorbin@cisco.com>
2017-04-20 17:06:43 -0500

    WORKING NavBar in App/index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import React from 'react';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import _ from 'lodash';
@@ -28,8 +29,33 @@ export default () => {
 
   return (
     <div id="container-fluid">
-      <div id="sidebar" />
-
+      <div id="Navbar">
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="http://foo.com">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="/jobs">Jobs</NavItem>
+              <NavItem eventKey={2} href="/">Users</NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="/link1">Link Right</NavItem>
+              <NavItem eventKey={2} href="/link2">Link Right</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
       <div className={styles.App}>
         <Helmet {...config.app} />
         <div className={styles.header}>
