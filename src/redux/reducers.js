5d87b57eb7f9baa88cb5fd0ba382300c4b407005
kecorbin <kecorbin@cisco.com>
2017-04-20 02:43:47 -0500

    add reducers for jobs/jobInfo

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,10 +4,15 @@ import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
 import home from '../containers/Home/reducer';
+import jobs from '../containers/JobsHome/reducer';
+
 import userInfo from '../containers/UserInfo/reducer';
+import jobInfo from '../containers/JobInfo/reducer';
 
 export default combineReducers({
   home,
+  jobs,
   userInfo,
+  jobInfo,
   router,
 });
