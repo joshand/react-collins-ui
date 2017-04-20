76ae30c2106b7ac0620e5cc11b3d225c3cb19d34
kecorbin <kecorbin@cisco.com>
2017-04-20 02:44:47 -0500

    add types, actions

--- src/types/index.js
+++ src/types/index.js
@@ -8,8 +8,23 @@ export type Home = {
   list: Array<Object>,
 };
 
+
+export type JobsHome = {
+  readyStatus: string,
+  err: any,
+  list: Array<Object>,
+};
+
 export type UserInfo = {
   [userId: string]: {
+    readyStatus: string,
+    err: any,
+    info: Object,
+  },
+};
+
+export type JobInfo = {
+  [jobId: string]: {
     readyStatus: string,
     err: any,
     info: Object,
@@ -22,6 +37,7 @@ export type Reducer = {
   router: any,
 };
 
+
 export type Action =
   { type: 'USERS_REQUESTING' } |
   { type: 'USERS_SUCCESS', data: Array<Object> } |
@@ -30,9 +46,19 @@ export type Action =
   { type: 'USER_SUCCESS', userId: string, data: Object } |
   { type: 'USER_FAILURE', userId: string, err: any };
 
+export type JobAction =
+  { type: 'JOBS_REQUESTING' } |
+  { type: 'JOBS_SUCCESS', data: Array<Object> } |
+  { type: 'JOBS_FAILURE', err: any } |
+  { type: 'JOB_REQUESTING', userId: string } |
+  { type: 'JOB_SUCCESS', userId: string, data: Object } |
+  { type: 'JOB_FAILURE', userId: string, err: any };
+
+
 export type Store = ReduxStore<Reducer, Action>;
 // eslint-disable-next-line no-use-before-define
-export type Dispatch = (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
+export type Dispatch = (action: Action | JobAction | ThunkAction | PromiseAction | Array<Action>) => any;
 export type GetState = () => Object;
 export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 export type PromiseAction = Promise<Action>;
+export type PromiseJobAction = Promise<JobAction>;
