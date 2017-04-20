466b08a263de1ec25480faf68923fee80ab10c49
kecorbin <kecorbin@cisco.com>
2017-04-20 10:23:48 -0500

    add boostrap from CDN

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -24,7 +24,8 @@ const Html = ({ store, htmlContent }: Props) => {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossOrigin="anonymous" />
         {head.title.toComponent()}
         {head.base.toComponent()}
         {head.meta.toComponent()}
